01fe17631527567d030357edca35a225d55e9b0d
Matthias Codvelle-Prime <matthias.codvelle@le-campus-numerique.fr>
2021-02-25 17:19:59 +0100

    Filtres [OK]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
-import CitiesListStoreLocalStorage from "../components/CitiesListStoreLocalStorage.vue";
+import CitiesListStoreLocalStorageFilters from "../components/CitiesListStoreLocalStorageFilters.vue";
 import CitiesMapLeafletStore from "../components/CitiesMapLeafletStore.vue";
 import City from "../components/City.vue";
 import EasterEgg from "../components/EasterEgg.vue";
 
 const routes = [
-  { path: '/villes', component: CitiesListStoreLocalStorage },
+  { path: '/villes', component: CitiesListStoreLocalStorageFilters },
   { path: '/carte', component: CitiesMapLeafletStore },
   { path: '/ville', component: City },
   { path: '/onenagros', component: EasterEgg}
