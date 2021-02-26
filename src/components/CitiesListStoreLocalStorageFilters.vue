f7bd4bdd4d40276c0ad58a56a439b315dae753e1
Matthias Codvelle-Prime <matthias.codvelle@le-campus-numerique.fr>
2021-02-26 10:45:01 +0100

    refacto

--- src/components/CitiesListStoreLocalStorageFilters.vue
+++ src/components/CitiesListStoreLocalStorageFilters.vue
@@ -2,16 +2,14 @@
   <h1>Liste des villes</h1>
   <br /><h3>Filtré la température : </h3>
   <input type="number" placeholder="Search..." v-model="input" v-on:input="search()" ref="input"><br/>
-  <div v-if="search().length > 0">
-    <City v-for="city of search() " :key="city.id" :name="city.name" :weather="city.weather"
-          :temperature="city.temperature" :updated-at="city.updatedAt"></City>
-  </div>
 
+  <div v-if="search().length != 0">
+  <City  v-for="city of search()" :key="city.id" :name="city.name" :weather="city.weather"
+        :temperature="city.temperature" :updated-at="city.updatedAt"></City>
+  </div>
   <div v-else>
-    <City v-for="city of cities " :key="city.id" :name="city.name" :weather="city.weather"
-          :temperature="city.temperature" :updated-at="city.updatedAt"></City>
+    <p>Aucun résultat pour ce filtre n'est actuellement disponible ..</p>
   </div>
-
 </template>
 
 <script lang="ts">
@@ -24,6 +22,38 @@ export default defineComponent({
   components: {
     City
   },
+  /* Lifecycle Diagram
+  beforeCreate() {
+    console.log('beforeCreate : ');
+    console.log(this.$el);
+    console.log('***************');
+  },
+  created() {
+    console.log('created : ');
+    console.log(this.$el);
+    console.log('***************');
+  },
+  beforeMount() {
+    console.log('beforeMount : ');
+    console.log(this.$el);
+    console.log('***************');
+  },
+  mounted() {
+    console.log('mounted : ');
+    console.log(this.$el);
+    console.log('***************');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate : ');
+    console.log(this.$el);
+    console.log('***************');
+  },
+  updated() {
+    console.log('updated : ');
+    console.log(this.$el);
+    console.log('***************');
+  },
+  */
   setup() {
     const store = useStore();
     store.dispatch('getCitiesAsync');
@@ -42,7 +72,6 @@ export default defineComponent({
           allCities.push(city)
         }
       }
-      console.log(allCities)
       return allCities
     }
   }
